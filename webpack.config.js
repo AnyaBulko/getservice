01866735e7872555c7b8183a2e9965905a334560
Anya <anya_bulko@mail.ru>
2022-12-09 00:38:37 +0300

    Подключение webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    // файл, с которого собирается исходный код
+    entry: path.join(__dirname, 'src', 'main', 'resources', 'static', 'js', 'main.js'),
+    devServer: {
+        // по умолчанию скомпилированные файлы будут тут
+        static: './dist',
+        compress: true,
+        port: 8000,
+        allowedHosts: [
+            'localhost:8080'
+        ]
+    },
+    module: {
+        rules: [
+            // через babel проходят js файлы
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            // vue через vue-loader
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    //
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src', 'main', 'resources', 'static', 'js'),
+            path.join(__dirname, 'node_modules'),
+        ],
+    }
+}
